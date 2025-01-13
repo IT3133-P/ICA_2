1b7dd568a03b7a45f8c2643ac83881b0e48da202
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-01-14 04:11:56 +0530

    feat: create bottom tab navbar

---
+++ samplemobileapp/components/navBar.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from '../page/Home';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Student from '../page/Student';
+
+
+export default function NavBar() {
+
+  const Stack = createBottomTabNavigator() 
+  return (
+          <Stack.Navigator>
+            <Stack.Screen name='home' component={Home} options={{title:'Home'}}/> 
+            <Stack.Screen name='student' component={Student} options={{title:'Student'}}/> 
+          </Stack.Navigator>
+
+  );
+}
+
+
+
